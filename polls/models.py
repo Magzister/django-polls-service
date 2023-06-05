99e8ff930b6e15eacefef8ea29b2b319f9ea3a14
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2023-06-05 18:14:14 +0300

    Created models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Question(models.Model):
+    text = models.TextField()
+    poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    is_correct = models.BooleanField()
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
