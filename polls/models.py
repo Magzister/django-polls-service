b3c91f94235af19be7fc48bfdb206c317c536ea2
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2023-06-05 18:15:56 +0300

    Add table names to db

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,21 @@ class Poll(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    class Meta:
+        db_table = 'poll'
 
 class Question(models.Model):
     text = models.TextField()
     poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'question'
 
 
 class Answer(models.Model):
     text = models.TextField()
     is_correct = models.BooleanField()
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'answer'
