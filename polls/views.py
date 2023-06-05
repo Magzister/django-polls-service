0e7ad758a1aa05ec8d25ca3f091be34e09d92e2d
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2023-06-05 18:29:36 +0300

    Created views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Poll, Question
+from .serializers import PollSerializer, QuestionSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PollList(generics.ListAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class QuestionList(generics.ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
